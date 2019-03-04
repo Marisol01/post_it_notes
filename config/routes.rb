cde38672c5408394b33ca2e5c7464c2932aa4600
Marisoln Araujo <u167272@gmail.com>
2019-03-03 19:37:37 -0700

    modified static page

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   # get 'note/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_notes#home'
+  root 'static_pages#home'
 
-  get '/about', to: 'static_notes#about'
+  get '/about', to: 'static_pages#about'
 
   resources :notes
 
