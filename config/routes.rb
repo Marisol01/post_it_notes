40ff4f579106e231ec30c9a0286a8e74a11c0733
Marisoln Araujo <u167272@gmail.com>
2019-03-03 17:33:40 -0700

    adding methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'note/index'
+  # get 'note/show'
+  # get 'note/new'
+  # get 'note/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'static_notes#home'
+
+  get '/about', to: 'static_notes#about'
+
+  resources :notes
+
+
 end
