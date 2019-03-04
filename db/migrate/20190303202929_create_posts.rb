f012dcbd5d3678d7b36b60ac9f4e5770193e3992
Marisoln Araujo <u167272@gmail.com>
2019-03-03 18:27:39 -0700

    modified app added navbar

--- db/migrate/20190303202929_create_posts.rb
+++ db/migrate/20190303202929_create_posts.rb
@@ -1,10 +1,10 @@
-class CreatePosts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :posts do |t|
-      t.string :it
-      t.string :Notes
+# class CreatePosts < ActiveRecord::Migration[5.2]
+#   def change
+#     create_table :posts do |t|
+#       t.string :it
+#       t.string :Notes
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
